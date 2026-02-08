9b45712ba82b023b142bc7e9af611cc761b14b34
Dor Haboosha <134487550+dorhaboosha@users.noreply.github.com>
2026-02-08 10:39:35 +0200

    Adding documentation to ElectricEngine file

--- GarageLogic/ElectricEngine.cs
+++ GarageLogic/ElectricEngine.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace GarageLogic
 {
+    /// <summary>
+    /// Represents an electric engine (battery) for electric vehicles.
+    /// </summary>
     public class ElectricEngine : Engine
     {
+        /// <summary>
+        /// Creates a new electric engine with the specified maximum battery capacity.
+        /// </summary>
+        /// <param name="i_MaxEngineAmount">Maximum battery capacity.</param>
         internal ElectricEngine(float i_MaxEngineAmount)
             : base(i_MaxEngineAmount)
         {
