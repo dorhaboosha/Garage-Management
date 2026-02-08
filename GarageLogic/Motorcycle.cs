4e845c7d68e09dab98ee2e266ee72e61787fae97
Dor Haboosha <134487550+dorhaboosha@users.noreply.github.com>
2026-02-08 11:14:28 +0200

    Adding documentation to Motorcycle file

--- GarageLogic/Motorcycle.cs
+++ GarageLogic/Motorcycle.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,16 +6,34 @@ using System.Threading.Tasks;
 
 namespace GarageLogic
 {
+    /// <summary>
+    /// Represents a motorcycle vehicle. Supports both fuel and electric variants via the engine type.
+    /// </summary>
     public class Motorcycle : Vehicle
     {
         private eMotorcycleLicenseType m_LicenseType;
         private int m_EngineVolume;
 
+        /// <summary>
+        /// Creates a new motorcycle with the given license number, engine, and wheels.
+        /// </summary>
+        /// <param name="i_LicenseNumber">The vehicle's license plate number.</param>
+        /// <param name="i_Engine">The engine (fuel or electric) for the motorcycle.</param>
+        /// <param name="i_Wheels">The wheels array for the motorcycle.</param>
         internal Motorcycle(string i_LicenseNumber, Engine i_Engine, Wheel[] i_Wheels)
             : base(i_LicenseNumber, i_Engine, i_Wheels)
         {
         }
 
+        /// <summary>
+        /// Sets the motorcycle's properties including model, wheels, license type, engine volume, and energy level.
+        /// </summary>
+        /// <param name="i_ModelName">The model name of the motorcycle.</param>
+        /// <param name="i_WheelManufacturerName">The manufacturer of the wheels.</param>
+        /// <param name="i_CurrentAirPressure">The current air pressure in the wheels.</param>
+        /// <param name="i_LicenseType">The required license type for the motorcycle.</param>
+        /// <param name="i_EngineVolume">The engine volume in cubic centimeters.</param>
+        /// <param name="i_CurrentAmountEnergy">The current fuel or battery level.</param>
         public void SetMotorcycleProperties(string i_ModelName, string i_WheelManufacturerName, float i_CurrentAirPressure,
             eMotorcycleLicenseType i_LicenseType, int i_EngineVolume, float i_CurrentAmountEnergy)
         {
@@ -24,6 +42,9 @@ namespace GarageLogic
             m_LicenseType = i_LicenseType;
         }
 
+        /// <summary>
+        /// Gets the license type as a string.
+        /// </summary>
         private string licenseType
         {
             get 
@@ -32,6 +53,9 @@ namespace GarageLogic
             }
         }
 
+        /// <summary>
+        /// Gets the engine volume in cubic centimeters.
+        /// </summary>
         private int engineVolume
         {
             get 
@@ -40,6 +64,10 @@ namespace GarageLogic
             }
         }
 
+        /// <summary>
+        /// Returns a string representation of the motorcycle, including license type, engine volume, and base vehicle details.
+        /// </summary>
+        /// <returns>Formatted motorcycle information.</returns>
         public override string ToString()
         {
             string motorcycleInfo = string.Format("Motorcycle License Type : {0} | Motorcycle Engine Volume : {1}\n{2}",
