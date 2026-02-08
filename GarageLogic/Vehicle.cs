137d91cac872f466c2e37ce697ead662e9a2a784
Dor Haboosha <134487550+dorhaboosha@users.noreply.github.com>
2026-02-08 11:24:36 +0200

    Adding documentation to Vehicle file

--- GarageLogic/Vehicle.cs
+++ GarageLogic/Vehicle.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace GarageLogic
 {
+    /// <summary>
+    /// Base class for all vehicles. Manages model, license, engine, wheels, and energy percentage.
+    /// Implemented by <see cref="Car"/>, <see cref="Motorcycle"/>, and <see cref="Truck"/>.
+    /// </summary>
     public class Vehicle
     {
         private string m_ModelName;
@@ -14,6 +18,13 @@ namespace GarageLogic
         private readonly Engine r_engine;
         private readonly Wheel[] r_wheels;
 
+        /// <summary>
+        /// Creates a new vehicle with the given license number, engine, and wheels.
+        /// </summary>
+        /// <param name="i_LicenseNumber">The vehicle's license plate number.</param>
+        /// <param name="i_Engine">The engine (fuel or electric) for the vehicle.</param>
+        /// <param name="i_Wheels">The wheels array for the vehicle.</param>
+        /// <exception cref="FormatException">Thrown when the license number is null or empty.</exception>
         public Vehicle(string i_LicenseNumber, Engine i_Engine, Wheel[] i_Wheels)
         {
             if (string.IsNullOrEmpty(i_LicenseNumber))
@@ -26,6 +37,13 @@ namespace GarageLogic
             r_wheels = i_Wheels;
         }
 
+        /// <summary>
+        /// Sets the vehicle's common properties: model name, wheel details, and current energy level.
+        /// </summary>
+        /// <param name="i_ModelName">The model name of the vehicle.</param>
+        /// <param name="i_WheelManufacrurerName">The manufacturer of the wheels.</param>
+        /// <param name="i_WheelCurrentAirPressure">The current air pressure in the wheels.</param>
+        /// <param name="i_CurrentAmountEnergy">The current fuel or battery level.</param>
         internal void SetVehicleProperties(string i_ModelName, string i_WheelManufacrurerName, 
             float i_WheelCurrentAirPressure,float i_CurrentAmountEnergy)
         {
@@ -34,12 +52,21 @@ namespace GarageLogic
             setWheelProperties(i_WheelManufacrurerName, i_WheelCurrentAirPressure);
         }
 
+        /// <summary>
+        /// Sets the engine's current energy level and updates the energy percentage.
+        /// </summary>
+        /// <param name="i_CurrentAmountEnergy">The current fuel or battery level to set.</param>
         private void setEngineCurrentAmountOfEnergy(float i_CurrentAmountEnergy)
         {
             Engine.FillingEnergyInEngine(i_CurrentAmountEnergy);
             UpdatingEnergyPrecentage();
         }
 
+        /// <summary>
+        /// Sets the manufacturer and air pressure for all wheels.
+        /// </summary>
+        /// <param name="i_WheelManufacrurerName">The wheel manufacturer name.</param>
+        /// <param name="i_WheelCurrentAirPressure">The current air pressure to set.</param>
         private void setWheelProperties(string i_WheelManufacrurerName, float i_WheelCurrentAirPressure)
         {
             foreach (Wheel wheel in Wheels)
@@ -48,6 +75,9 @@ namespace GarageLogic
             }
         }
 
+        /// <summary>
+        /// Gets the model name.
+        /// </summary>
         private string modelName
         {
             get
@@ -56,6 +86,9 @@ namespace GarageLogic
             }
         }
 
+        /// <summary>
+        /// Gets the license plate number.
+        /// </summary>
         public string LicenseNumber
         {
             get
@@ -64,6 +97,9 @@ namespace GarageLogic
             }
         }
 
+        /// <summary>
+        /// Gets the vehicle's engine (fuel or electric).
+        /// </summary>
         public Engine Engine
         {
             get
@@ -72,6 +108,9 @@ namespace GarageLogic
             }
         }
 
+        /// <summary>
+        /// Gets the vehicle's wheels.
+        /// </summary>
         private Wheel[] Wheels
         {
             get 
@@ -80,11 +119,17 @@ namespace GarageLogic
             }
         }
 
+        /// <summary>
+        /// Recalculates the remaining energy percentage based on current vs. max engine capacity.
+        /// </summary>
         internal void UpdatingEnergyPrecentage()
         {
             m_RemainingEnergyPercentage = (r_engine.CurrentEngineAmount / r_engine.MaxEngineAmount) * 100;
         }
 
+        /// <summary>
+        /// Inflates all wheels to their maximum pressure.
+        /// </summary>
         internal void InflateTires()
         {
             foreach (Wheel wheel in Wheels)
@@ -93,6 +138,10 @@ namespace GarageLogic
             }
         }
 
+        /// <summary>
+        /// Returns a string with model, license, energy percentage, engine info, and wheel details.
+        /// </summary>
+        /// <returns>Formatted vehicle information.</returns>
         public override string ToString()
         {
             string vehicleInfo = string.Format("Vehicle's information -\n" +
