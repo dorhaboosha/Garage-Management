b2c3c536b552315c0ac8c3ece22eaaa184b7cc7c
Dor Haboosha <134487550+dorhaboosha@users.noreply.github.com>
2026-02-08 11:10:23 +0200

    Adding documentation to FuelEngine file

--- GarageLogic/FuelEngine.cs
+++ GarageLogic/FuelEngine.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,16 +6,27 @@ using System.Threading.Tasks;
 
 namespace GarageLogic
 {
+    /// <summary>
+    /// Represents a fuel-powered engine for vehicles that use gasoline/diesel.
+    /// </summary>
     public class FuelEngine : Engine
     {
         private readonly eFuelType r_FuelType;
 
+        /// <summary>
+        /// Creates a new fuel engine with the specified capacity and fuel type.
+        /// </summary>
+        /// <param name="i_MaxEngineAmount">Maximum fuel tank capacity.</param>
+        /// <param name="i_FuelType">The type of fuel this engine uses.</param>
         internal FuelEngine(float i_MaxEngineAmount, eFuelType i_FuelType)
             :base(i_MaxEngineAmount)
         {
            r_FuelType = i_FuelType;
         }
 
+        /// <summary>
+        /// Gets the fuel type this engine uses.
+        /// </summary>
         internal string FuelType
         {
             get 
@@ -24,6 +35,10 @@ namespace GarageLogic
             }
         }
 
+        /// <summary>
+        /// Returns a string with the fuel type and base engine information.
+        /// </summary>
+        /// <returns>Formatted fuel engine information.</returns>
         public override string ToString()
         {
             string FuelEngineInfo = string.Format("Fuel Type : {0}\n{1}", FuelType, base.ToString());
