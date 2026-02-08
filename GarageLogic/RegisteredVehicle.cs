2a56a85a3365332dfb7d55594c38b9539dbd04b3
Dor Haboosha <134487550+dorhaboosha@users.noreply.github.com>
2026-02-08 11:18:28 +0200

    Adding documentation to RegisteredVehicle file

--- GarageLogic/RegisteredVehicle.cs
+++ GarageLogic/RegisteredVehicle.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,17 +6,28 @@ using System.Threading.Tasks;
 
 namespace GarageLogic
 {
+    /// <summary>
+    /// Combines a vehicle with its garage ticket, representing a vehicle registered in the garage.
+    /// </summary>
     public class RegisteredVehicle
     {
         private readonly Vehicle r_Vehicle;
         private readonly GarageTicket r_GarageTicket;
 
+        /// <summary>
+        /// Creates a new registered vehicle pairing a vehicle with its garage ticket.
+        /// </summary>
+        /// <param name="i_Vehicle">The vehicle.</param>
+        /// <param name="i_OwnerVehicleInfo">The garage ticket with owner and status information.</param>
         internal RegisteredVehicle(Vehicle i_Vehicle, GarageTicket i_OwnerVehicleInfo)
         {
             r_Vehicle = i_Vehicle;
             r_GarageTicket = i_OwnerVehicleInfo;
         }
 
+        /// <summary>
+        /// Gets the vehicle.
+        /// </summary>
         public Vehicle Vehicle
         {
             get
@@ -25,6 +36,9 @@ namespace GarageLogic
             }
         }
 
+        /// <summary>
+        /// Gets the garage ticket with owner and status information.
+        /// </summary>
         public GarageTicket GarageTicketInfo
         {
             get 
@@ -33,6 +47,10 @@ namespace GarageLogic
             }
         }
 
+        /// <summary>
+        /// Returns a string combining the garage ticket info and vehicle details.
+        /// </summary>
+        /// <returns>Formatted registered vehicle information.</returns>
         public override string ToString()
         {
             string registeredCar = string.Format("{0}\n{1}", GarageTicketInfo.ToString(), Vehicle.ToString());
